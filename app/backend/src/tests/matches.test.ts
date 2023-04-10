f59b5c34f9eef7f166f42dbe4c56923e4c2d6edf
wanderson-rpf <wandersonrpf@gmail.com>
2023-04-09 23:31:47 -0300

    :test_tube: [test] Tests and mocks route matches.

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -5,6 +5,7 @@ import { Model } from 'sequelize';
 import { app } from '../app';
 import { 
   listAllMatches, 
+  match, 
   matchesInProgressFalse, 
   matchesInProgressTrue,
 } from './mocks/matches.mock';
@@ -22,42 +23,6 @@ describe("Test route /MATCHES", () => {
       expect(httpResponse.status).to.equal(500);
       expect(httpResponse.body).to.deep.equal({ message: "Invalid parameter" });
     })
-
-    // it('1.2) Test POST /LOGIN with invalid email.', async () => {
-    //   const httpResponse = await chai.request(app).post('/login').send({
-    //     email: "admin-admin.com",
-    //     password: "secret_admin"
-    //   });
-    //   expect(httpResponse.status).to.equal(401);
-    //   expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
-    // })
-
-    // it('1.3) Test POST /LOGIN with invalid password.', async () => {
-    //   const httpResponse = await chai.request(app).post('/login').send({
-    //     email: "admin@admin.com",
-    //     password: "sec"
-    //   });
-    //   expect(httpResponse.status).to.equal(401);
-    //   expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
-    // })
-
-    // it('1.4) Test POST /LOGIN with non-existent email in the database.', async () => {
-    //   const httpResponse = await chai.request(app).post('/login').send({
-    //     email: "adminAdmin@admin.com",
-    //     password: "secret_admin"
-    //   });
-    //   expect(httpResponse.status).to.equal(401);
-    //   expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
-    // })
-
-    // it('1.5) Test POST /LOGIN with non-existent password in the database.', async () => {
-    //   const httpResponse = await chai.request(app).post('/login').send({
-    //     email: "admin@admin.com",
-    //     password: "secret_adminA"
-    //   });
-    //   expect(httpResponse.status).to.equal(401);
-    //   expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
-    // })
   });
 
   describe('2.0) Test success case in GET /MATCHES. ', () => {
@@ -69,7 +34,7 @@ describe("Test route /MATCHES", () => {
       expect(httpResponse.body).to.deep.equal(listAllMatches);
     })
 
-    it('2.2) Test GET /MATCHES that filter progress query equal true.', async () => {
+    it('2.2) Test PATCH /MATCHES that filter progress query equal true.', async () => {
       const httpResponse = await chai
         .request(app)
         .get('/matches')
@@ -87,42 +52,25 @@ describe("Test route /MATCHES", () => {
     })
   });
 
-  // describe('3.0) Test error case in GET /LOGIN/ROLE. ', () => {
-  //   it('3.1) Test GET /LOGIN/ROLE with empty token in header.', async () => {
+  // describe('3.0) Test error case in PATCH /MATCHES. ', () => {
+  //   it('3.1) Test PATCH /MATCHES invalid endpoint.', async () => {
+  //     sinon.stub(Model, 'update').resolves(match)
   //     const httpResponse = await chai
   //       .request(app)
-  //       .get('/login/role');
-  //     expect(httpResponse.status).to.equal(401);
-  //     expect(httpResponse.body).to.deep.equal({ message: "Token not found" })
-  //   })
-
-  //   it('3.2) Test GET /LOGIN/ROLE with invalid token in header.', async () => {
-  //     const httpResponse = await chai
-  //       .request(app)
-  //       .get('/login/role')
-  //       .set({ 'Authorization': tokenInvalid }); // Use set function to set http headers.
-  //     expect(httpResponse.status).to.equal(401);
-  //     expect(httpResponse.body).to.deep.equal({ message: "Token must be a valid token" })
-  //   })
-
-  //   it('3.3) Test GET /LOGIN/ROLE with expired token in header.', async () => {
-  //     const httpResponse = await chai
-  //       .request(app)
-  //       .get('/login/role')
-  //       .set({ 'Authorization': tokenExpired }); // Use set function to set http headers.
-  //     expect(httpResponse.status).to.equal(500);
-  //     expect(httpResponse.body).to.deep.equal({ message: "WHERE parameter \"email\" has invalid \"undefined\" value" })
-  //   })
-  // });
-
-  // describe('4.0) Test success case in GET /LOGIN/ROLE. ', () => {
-  //   it('4.1) Test POST /LOGIN with valid data.', async () => {
-  //     const httpResponse = await chai
-  //       .request(app)
-  //       .get('/login/role')
-  //       .set({ 'Authorization': token }); // Use set function to set http headers.
+  //       .patch('/matches/1');
   //     expect(httpResponse.status).to.equal(200);
-  //     expect(httpResponse.body).to.deep.equal({ role: "admin" })
+  //     expect(httpResponse.body).to.deep.equal({ message: "Finished" })
   //   })
   // });
+
+  describe('4.0) Test success case in PATCH /MATCHES. ', () => {
+    it('4.1) Test PATCH /MATCHES update status match.', async () => {
+      sinon.stub(Model, 'update').resolves(match)
+      const httpResponse = await chai
+        .request(app)
+        .patch('/matches/1/finish');
+      expect(httpResponse.status).to.equal(200);
+      expect(httpResponse.body).to.deep.equal({ message: "Finished" })
+    })
+  });
 });
