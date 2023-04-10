da7202a4dbdc21a392374ffcb6ac2d999dae624c
wanderson-rpf <wandersonrpf@gmail.com>
2023-04-10 01:15:03 -0300

    :test_tube: [test] Tests and mocks route login.

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -9,43 +9,58 @@ const { expect } = chai;
 describe("Test route /LOGIN", () => {
   describe('1.0) Test error case in POST /LOGIN. ', () => {
     it('1.1) Test POST /LOGIN with empty data.', async () => {
-      const httpResponse = await chai.request(app).post('/login').send({});
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({});
       expect(httpResponse.status).to.equal(400);
       expect(httpResponse.body).to.deep.equal({ message: "All fields must be filled" })
     })
 
     it('1.2) Test POST /LOGIN with invalid email.', async () => {
-      const httpResponse = await chai.request(app).post('/login').send({
-        email: "admin-admin.com",
-        password: "secret_admin"
-      });
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: "admin-admin.com",
+          password: "secret_admin"
+        });
       expect(httpResponse.status).to.equal(401);
       expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
     })
 
     it('1.3) Test POST /LOGIN with invalid password.', async () => {
-      const httpResponse = await chai.request(app).post('/login').send({
-        email: "admin@admin.com",
-        password: "sec"
-      });
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: "admin@admin.com",
+          password: "sec"
+        });
       expect(httpResponse.status).to.equal(401);
       expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
     })
 
     it('1.4) Test POST /LOGIN with non-existent email in the database.', async () => {
-      const httpResponse = await chai.request(app).post('/login').send({
-        email: "adminAdmin@admin.com",
-        password: "secret_admin"
-      });
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: "adminAdmin@admin.com",
+          password: "secret_admin"
+        });
       expect(httpResponse.status).to.equal(401);
       expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
     })
 
     it('1.5) Test POST /LOGIN with non-existent password in the database.', async () => {
-      const httpResponse = await chai.request(app).post('/login').send({
-        email: "admin@admin.com",
-        password: "secret_adminA"
-      });
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: "admin@admin.com",
+          password: "secret_adminA"
+        });
       expect(httpResponse.status).to.equal(401);
       expect(httpResponse.body).to.deep.equal({ message: "Invalid email or password" })
     })
@@ -53,10 +68,13 @@ describe("Test route /LOGIN", () => {
 
   describe('2.0) Test success case in POST /LOGIN. ', () => {
     it('2.1) Test POST /LOGIN with valid data.', async () => {
-      const httpResponse = await chai.request(app).post('/login').send({
-        email: "admin@admin.com",
-        password: "secret_admin"
-      });
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: "admin@admin.com",
+          password: "secret_admin"
+        });
       expect(httpResponse.status).to.equal(200);
       expect(httpResponse.body).to.have.property('token');
     })
